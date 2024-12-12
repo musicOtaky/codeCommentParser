6f66e503d42d953916cdfe8785927ea03bd9db07
musicOtaky <elkintema@icloud.com>
2024-12-12 17:21:19 +0100

    fix: fix test to run correctly

--- tests/expected_output.c
+++ tests/expected_output.c
@@ -8,14 +8,14 @@ int add(int a, int b) {
 
 int main() {
     printf("Starting the program...\n");
-    
+
     int x = 5;
     int y = 10;
     int sum = add(x, y); 
     printf("The sum of %d and %d is %d\n", x, y, sum);
-    
+
     printf("This line had some comments.\n"); 
-    
-    
+
+
     return 0; 
 }
